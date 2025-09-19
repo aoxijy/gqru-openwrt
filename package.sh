82556317430dc66d7378fccd604f374124eaacee
Cai <106552747+aoxijy@users.noreply.github.com>
2025-09-20 06:24:12 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,9 +3,7 @@ git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/Open
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 
-# Theme
-# luci-theme-neobird
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
+
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

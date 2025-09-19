d62cb494d0202ab1d76fa272b4c8b418063bf4b8
Cai <106552747+aoxijy@users.noreply.github.com>
2025-09-20 03:44:00 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -10,8 +10,5 @@ git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/o
 # Mentohust
 git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
 
-# UnblockNeteaseMusic
-# git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
-
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

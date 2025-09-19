f59f1280c183b031effdc364d237d366f9d5b5ca
Cai <106552747+aoxijy@users.noreply.github.com>
2025-09-20 03:45:26 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -7,8 +7,5 @@ git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherap
 # luci-theme-neobird
 git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
 
-# Mentohust
-git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
-
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

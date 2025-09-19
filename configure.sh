d9a9c676fd51b465311fdc5e5f6447cee2d2d556
Cai <106552747+aoxijy@users.noreply.github.com>
2025-09-20 00:38:47 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 
 # iStore
-echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+# echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 

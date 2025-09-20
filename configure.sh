d5ec01b17cf8539e2e20d985c2f1704bc736d821
Cai <106552747+aoxijy@users.noreply.github.com>
2025-09-20 13:02:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,7 +4,7 @@
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
-# 添加额外的软件源
+# 添加额外的软件源（确保不要重复添加luci）
 echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird.git' >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
@@ -13,8 +13,6 @@ echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
 rm -rf package/lean/luci-theme-argon 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# 添加缺失的依赖包源
-echo "src-git luci https://github.com/coolsnowwolf/luci" >> feeds.conf.default
-echo "src-git packages https://github.com/coolsnowwolf/packages" >> feeds.conf.default
-echo "src-git routing https://git.openwrt.org/feed/routing.git" >> feeds.conf.default
-echo "src-git telephony https://git.openwrt.org/feed/telephony.git" >> feeds.conf.default
+# 移除可能重复的luci源添加
+# 不要重复添加以下内容：
+# echo "src-git luci https://github.com/coolsnowwolf/luci" >> feeds.conf.default
